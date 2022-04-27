b84a848d0f1ebc5e1dcef6d2b97feadcc1c7f13a
Jimmy Senecal <jamesnsenecal@gmail.com>
2022-04-27 11:44:34 -0400

    commit tree 57aa420b11b162d2468dc1d42e40f5ce35160ca7

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function () {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function () {
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
